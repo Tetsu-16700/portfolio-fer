66be7012ff65c394983cdd1a27ca1ab541f0929f
Tetsu-16700 <carolina.moscosot@gmail.com>
2024-04-30 23:50:13 -0500

    edit portfolio

--- app/home/portfolio/portfolioFer.tsx
+++ app/home/portfolio/portfolioFer.tsx
@@ -4,14 +4,22 @@ function PortfolioPersonal() {
   return (
     <>
       {/* Seccion inicio */}
+      {/* Seccion inicio */}
+      {/* Seccion inicio */}
+      {/* Seccion inicio */}
       <section
         id="inicio"
-        className="h-screen bg-cover bg-center bg-black bg-opacity-90  "
+        className="h-screen bg-cover bg-center bg-black bg-opacity-100  "
         style={{
           backgroundImage:
-            "url(https://img.freepik.com/vector-premium/comunicacion-vector-fondo-red-digital-diseno-onda-tecnologia-azul-purpura-abstracto_501557-422.jpg)",
+            "url(https://img.freepik.com/premium-vector/abstract-background-random-digital-binary-number-programming_35887-375.jpg)",
         }}
       >
+        {/* <img
+          src="//public/porfoliodis.png"
+          alt="portfolio"
+          className=""
+        /> */}
         <div className=" flex items-center justify-center w-full ">
           <header className="fixed w-full z-50 top-0 bg-opacity-80 bg-black    ">
             <div className="flex justify-between  items-center py-4 bg-center ">
@@ -23,45 +31,45 @@ function PortfolioPersonal() {
                 <ul id="links" className="flex ">
                   <li>
                     <a
-                      //   href="#inicio"
+                      href="#inicio"
                       className="text-purple-700 mr-4  hover:text-purple-300 font-semibold "
-                      //   onclick="seleccionar(this)"
+                      // onClick={() => seleccionar(this)}
                     >
                       INICIO
                     </a>
                   </li>
                   <li>
                     <a
-                      //   href="#sobreMi"
+                      href="#sobreMi"
                       className="text-purple-700 mr-4 hover:text-purple-300 font-semibold"
-                      //   onclick="seleccionar(this)"
+                      // onClick={() => seleccionar(this)}
                     >
                       SOBRE MI
                     </a>
                   </li>
                   <li>
                     <a
-                      //   href="#servicios"
+                      href="#servicios"
                       className="text-purple-700 mr-4 hover:text-purple-300 font-semibold"
-                      //   onclick="seleccionar(this)"
+                      // onClick={() => seleccionar(this)}
                     >
                       SERVICIOS
                     </a>
                   </li>
                   <li>
                     <a
-                      //   href="#portfolio"
+                      href="#portfolio"
                       className="text-purple-700 mr-4 hover:text-purple-300 font-semibold"
-                      //   onclick="seleccionar(this)"
+                      // onClick={() => seleccionar(this)}
                     >
                       PORTFOLIO
                     </a>
                   </li>
                   <li>
                     <a
-                      //   href="#contacto"
+                      href="#contacto"
                       className="text-purple-700 mr-4 hover:text-purple-300 font-semibold"
-                      //   onclick="seleccionar(this)"
+                      // onClick={() => seleccionar(this)}
                     >
                       CONTACTO
                     </a>
@@ -87,12 +95,12 @@ function PortfolioPersonal() {
           </header>
 
           <div className="flex flex-col justify-center items-center h-screen">
-            <p className="text-lg mb-4 uppercase tracking-widest text-slate-900">
+            <p className="text-3xl mb-4 uppercase tracking-widest text-gray-800 font-mono">
               Bienvenidos
             </p>
-            <h2 className="text-purple-400 text-5xl font-medium">
-              Hola soy{" "}
-              <span className="text-violet-800 text-3xl font-semibold">
+            <h2 className="text-purple-400 text-5xl font-medium ">
+              Hola soy
+              <span className="text-indigo-800 text-4xl font-normal ml-5">
                 Fernanda
               </span>
               , Desarrolladora Web
@@ -113,25 +121,34 @@ function PortfolioPersonal() {
           </div>
         </div>
       </section>
+
+      {/* seccion sobre mi */}
+      {/* seccion sobre mi */}
+      {/* seccion sobre mi */}
       {/* seccion sobre mi */}
       <section id="sobreMi" className="py-20">
         <div className="mx-auto flex items-center">
           <img
             className="rounded-lg"
-            src="../Portafolio/assets/img/Me.jpg"
+            // src="../Portafolio/assets/img/Me.jpg"
             alt=""
           />
         </div>
+
         {/* sobreMi */}
-        <div className=" mx-auto max-w-screen-lg px-4 text-justify ">
+        {/* sobreMi */}
+        {/* sobreMi */}
+        {/* sobreMi */}
+
+        <div className=" mx- max-w-screen-lg px-4  ">
           <p className="text-2xl uppercase mb-4">Sobre Mi</p>
           <h2 className="text-4xl font-bold mb-2 ">
-            Hola, soy <span className="text-indigo-950">Fernanda</span>
+            Hola, soy <span className="text-indigo-950">Fernada</span>
           </h2>
           <h3 className="text-xl font-semibold mb-4">Desarrolladora Web</h3>
 
-          <p className="text-gray-500 mb-4 text-">
-            Mi inmersión en el intrigante universo de la tecnología se inició
+          <p className="text-gray-500 mb-4 text-r">
+            Mi inmersión en el alucinante universo de la tecnología se inició
             como una diminuta chispa de curiosidad ✨ para transformarse en una
             apasionada llama 🔥. Desde el primer día, mi ímpetu ha sido una
             insaciable sed de conocimiento 📚 y la incesante búsqueda de
@@ -139,9 +156,20 @@ function PortfolioPersonal() {
           </p>
           <p className="text-gray-500 mb-4">
             En calidad de Desarrollador Full Stack, he conquistado un extenso
-            panorama de tecnologías, abordando con mucha destreza: 🚀 Backend🚀:
-            JavaScript / Node.js Bases de datos: PostgreSQL, MySQL, SQLite.
-            🎨Frontend🎨: HTML5, CSS3 Tailwind CSS JavaScript React
+            panorama de tecnologías, abordando con mucha destreza:
+            <p>🚀 Backend🚀:</p>
+            <ul>
+              <li>JavaScript / Node.js</li>
+              <li>Bases de datos: PostgreSQL, MySQL, SQLite</li>
+            </ul>
+            <p>🎨Frontend🎨:</p>
+            <ul>
+              <li>HTML5</li>
+              <li>CSS</li>
+              <li>Tailwind</li>
+              <li>JavaScript</li>
+              <li>React</li>
+            </ul>
           </p>
           <p className="text-gray-500 mb-4">
             Si estás en la búsqueda de un apasionado de la tecnología, me
@@ -156,13 +184,17 @@ function PortfolioPersonal() {
           </a>
         </div>
       </section>
+
+      {/* seccion de servicios */}
+      {/* seccion de servicios */}
+      {/* seccion de servicios */}
       {/* seccion de servicios */}
       <section
         id="servicios"
         className="py-20 bg-purple-100"
         style={{
           backgroundImage:
-            "url(https://img.freepik.com/fotos-premium/composicion-procesamiento-codificacion-binaria-sobre-fondo-morado_1134-6048.jpg)",
+            "url(https://online.york.ac.uk/wp-content/uploads/2023/05/software.jpg)",
         }}
       >
         <h3 className="titulo-seccion text-2xl uppercase mb-12">
@@ -225,13 +257,25 @@ function PortfolioPersonal() {
       </section>
 
       {/* seccion de portafolio */}
+      {/* seccion de portafolio */}
+      {/* seccion de portafolio */}
+      {/* seccion de portafolio */}
       <section id="portfolio" className="px-4 py-20">
+        {/* titulo-seccion */}
+        {/* titulo-seccion */}
+        {/* titulo-seccion */}
         {/* titulo-seccion */}
         <h3 className="titulo-seccion text-2xl uppercase mb-12 text-center">
           MIS PROYECTOS
         </h3>
         {/* fila */}
+        {/* fila */}
+        {/* fila */}
+        {/* fila */}
         <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+          {/* proyecto */}
+          {/* proyecto */}
+          {/* proyecto */}
           {/* proyecto */}
           <div className=" relative overflow-hidden">
             <div className=" bg-black bg-opacity-75 absolute inset-0" />
@@ -243,17 +287,23 @@ function PortfolioPersonal() {
             </div>
           </div>
           {/* proyecto */}
+          {/* proyecto */}
+          {/* proyecto */}
+          {/* proyecto */}
           <div className="relative overflow-hidden">
             {/* overlay */}
             <div className="bg-black bg-opacity-75 absolute inset-0" />
             {/* aqui poner imagen de proyecto */}
-            {/* <img src="./assets/img/captura.png" alt="" className="w-full" /> */}
+            <img src="./assets/img/captura.png" alt="" className="w-full" />
             {/* info */}
             <div className="absolute bottom-0 left-0 w-full bg-black bg-opacity-75 text-white p-4 opacity-0 transition duration-300 ease-in-out">
               <h4>Descripcion del Proyecto</h4>
               <p>Diseño Web</p>
             </div>
           </div>
+          {/* proyecto */}
+          {/* proyecto */}
+          {/* proyecto */}
           {/* proyecto */}
           <div className="relative overflow-hidden">
             {/* overlay */}
@@ -269,6 +319,9 @@ function PortfolioPersonal() {
         {/* si exite varios proyectos aqui abajo incluirlos */}
       </section>
 
+      {/* seccion contacto */}
+      {/* seccion contacto */}
+      {/* seccion contacto */}
       {/* seccion contacto */}
       <section id="contacto" className="px-4 py-20">
         {/* titulo-seccion */}
@@ -325,6 +378,9 @@ function PortfolioPersonal() {
         </div>
       </section>
 
+      {/* seccion footer */}
+      {/* seccion footer */}
+      {/* seccion footer */}
       {/* seccion footer */}
       <footer className="bg-black text-white text-center py-8">
         <p className="mb-4">Todos los derechos reservados - 2023</p>
